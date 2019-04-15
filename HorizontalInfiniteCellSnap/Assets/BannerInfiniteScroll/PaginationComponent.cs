c0df61cf4117b99f51be50ab60974f40caec014a
Yathish Gk <yathishgk@gmail.com>
2019-04-15 19:01:40 +0530

    sample with unity package

---
+++ HorizontalInfiniteCellSnap/Assets/BannerInfiniteScroll/PaginationComponent.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.UI.Extensions;
+
+public class PaginationComponent : MonoBehaviour
+{
+    public HorizontalScrollSnap mHorizonatlScrollComp;
+
+    [SerializeField]
+    private Image[] mPageIndicators;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        mHorizonatlScrollComp.OnSelectionChangeEndEvent.AddListener(OnPageChanged);
+
+        OnPageChanged(0);
+    }
+
+    // Update is called once per frame
+    void OnPageChanged(int inPage)
+    {
+        Debug.Log("Page " + inPage);
+
+        for(int i=0;i<mPageIndicators.Length;i++)
+        {
+            if((inPage) == i)
+            {
+                mPageIndicators[i].color = Color.black;
+            }
+            else
+            {
+                mPageIndicators[i].color = Color.white;
+            }
+        }
+    }
+}
